28624255f40d219887fe85d1124089fbce9801c4
rachadnait <rachadsalazar1996@gmail.com>
2025-08-01 16:39:11 +0400

    admin panel hra steps app completed

--- src/plugins/axios/index.js
+++ src/plugins/axios/index.js
@@ -2,7 +2,8 @@
 import axios from "axios"
 
 const axiosIns = axios.create({
-        baseURL: "http://localhost:4000/api/"
+        // baseURL: "http://localhost:4000/api/"
+        baseURL: "https://backend.hra-steps.com/api/"
 })
 
 export default axiosIns;
